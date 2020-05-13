7d6e4dc8f294fd05615831d4039881a6d685492d
abhishek k <abhi281342@gmail.com>
2020-05-13 11:30:59 +0530

    Updates

---
+++ precache-manifest.be6d1d2c64afc4c67bbda07c8383bdeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de777674520b29dd02ed8f457de99c6a",
+    "url": "/abhishek9686/react-app.git/index.html"
+  },
+  {
+    "revision": "9192f9ae6805830070c0",
+    "url": "/abhishek9686/react-app.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "937b30c6a7c21c387031",
+    "url": "/abhishek9686/react-app.git/static/css/main.f6f567e4.chunk.css"
+  },
+  {
+    "revision": "9192f9ae6805830070c0",
+    "url": "/abhishek9686/react-app.git/static/js/2.5e0464f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abhishek9686/react-app.git/static/js/2.5e0464f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937b30c6a7c21c387031",
+    "url": "/abhishek9686/react-app.git/static/js/main.abe62bd8.chunk.js"
+  },
+  {
+    "revision": "81ce4eb817f413646bce",
+    "url": "/abhishek9686/react-app.git/static/js/runtime-main.92b5f26c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/abhishek9686/react-app.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
